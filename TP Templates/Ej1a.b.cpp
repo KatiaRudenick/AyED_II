0723bf6064d7b4268119ed9a2892905c94e5bae9
katiarudenick <katia.rnk@gmail.com>
2023-10-27 01:25:55 -0300

    Tp templates

--- TP Templates/Ej1a.b.cpp
+++ TP Templates/Ej1a.b.cpp
@@ -12,7 +12,6 @@ template <typename T>
 class MiClase {
 public:
     T miAtributo;
-
     //Constructor que inicializa miAtributo
     MiClase(T valorInicial) : miAtributo(valorInicial) {}
 
@@ -22,35 +21,27 @@ public:
 };
 
 int main() {
-    //Uso de la función de plantilla
     int entero = 5;
     double decimal = 2.5;
 
-    // Llamar a la funcion con diferentes tipos de datos
+    // Llamo a la funcion con diferentes tipos de datos
     int resultadoEntero = miFuncion<int>(entero);
     double resultadoDecimal = miFuncion<double>(decimal);
 
    cout << "Resultado con tipo int: " << resultadoEntero <<endl;
    cout << "Resultado con tipo double: " << resultadoDecimal <<endl;
 
-
-
-   // Crear una instancia de MiClase con un tipo de dato int
+   // Creao una instancia de MiClase con un tipo de dato int
     MiClase<int> objetoInt(6);
 
-    // Llamar a la función hacerAlgo
     objetoInt.hacerAlgo();
 
-    // Mostrar el valor de miAtributo
    cout << "Valor de miAtributo (int): " << objetoInt.miAtributo <<endl;
 
-    // Crear una instancia de MiClase con un tipo de dato double
     MiClase<double> objetoDouble(4.5);
 
-    // Llamar a la función hacerAlgo
     objetoDouble.hacerAlgo();
 
-    // Mostrar el valor de miAtributo
    cout << "Valor de miAtributo (double): " << objetoDouble.miAtributo <<endl;
 
   return 0;
--- TP Templates/Ej1c.cpp
+++ TP Templates/Ej1c.cpp
@@ -9,35 +9,28 @@ private:
 public:
     // Constructor que toma un argumento
     OtraClase(ConstructorArg arg) : dato(arg) {
-        // Constructor
     }
 
-    // Método que devuelve un valor de tipo ReturnType
+    //Metodo que devuelve un valor de tipo ReturnType
     ReturnType otroMetodo() {
-        // Implementación del método, por ejemplo, duplicar el valor del argumento
         ReturnType resultado = dato * 2;
         return resultado;
     }
 };
 
 int main() {
-    // Crear una instancia de OtraClase con un tipo de dato int y un valor inicial de 5
+    // Creo una instancia de OtraClase (objetoInt) con un tipo de dato int y un valor inicial de 5
     OtraClase<int, int> objetoInt(5);
 
-    // Llamar al método otroMetodo
     int resultadoInt = objetoInt.otroMetodo();
 
-    // Mostrar el resultado
-    std::cout << "Resultado (int): " << resultadoInt << std::endl;
+    cout << "Resultado (int): " << resultadoInt << endl;
 
-    // Crear una instancia de OtraClase con un tipo de dato double y un valor inicial de 2.5
     OtraClase<double, double> objetoDouble(2.5);
 
-    // Llamar al método otroMetodo
     double resultadoDouble = objetoDouble.otroMetodo();
 
-    // Mostrar el resultado
-    std::cout << "Resultado (double): " << resultadoDouble << std::endl;
+    cout << "Resultado (double): " << resultadoDouble << endl;
 
     return 0;
 }
